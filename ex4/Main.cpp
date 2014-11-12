d016625be0c014bbf9206a1fbda9b7cb5e67fec2
Raphael Bussa <raphaelbussa@gmail.com>
2014-11-12 22:07:00 +0100

    Modificata l'esercitazione 4

--- ex4/Main.cpp
+++ ex4/Main.cpp
@@ -4,26 +4,33 @@
 using namespace std;
 
 int main() {
-	cout << "Materiale\tDensita' (Kg/m^3)\tV ingr. in Acqua (m/s)\tCond. Finale" << endl;
+	double r = 0.5;
+	double volume = 4/3 * 3.14 * r * r * r;
 	double densita[4] = {150, 500, 2700, 8000};
+	double k1Aria = 0.00035;
+	double k1Acqua = 0.02;
+	double k2Aria = 0.84;
+	double k2Acqua = 700;
+	double fIdroAria;
+	double fIdroAcqua;
+	double massa;
+	double fy;
+	double altezza;
+	double velocitaIngresso;
+	double fArchimedeAcqua;
+	double forzaDaUsare;
 	string materiale[4] = {"Gomma Rigida", "Legno    ", "Alluminio", "Ferro    "};
+	cout << "Materiale\tDensita' (Kg/m^3)\tV ingr. in Acqua (m/s)\tCond. Finale" << endl;
 	for (int i=0; i<4; i++) {
-		double r = 0.5;
-		double volume = 4/3 * 3.14 * r * r * r;
-		double massa = densita[i] * volume;
+		massa = densita[i] * volume;
 		CorpoCeleste pallina ("Pallina", massa, 0, 100, 0, 0);
-		double altezza = pallina.Y();
-		double fy = pallina.M() * -9.81;
-		double k1Aria = 0.00035;
-		double k1Acqua = 0.02;
-		double k2Aria = 0.84;
-		double k2Acqua = 700;
-		double fIdroAria = k1Aria * r * abs(pallina.Vy()) + k2Aria * r * r * (pallina.Vy() * pallina.Vy());
-		double fIdroAcqua = k1Acqua * r * abs(pallina.Vy()) + k2Acqua * r * r * (pallina.Vy() * pallina.Vy());
-		double fArchimedeAcqua = 1000 * volume * 9.81;
-		double velocitaIngresso;
+		altezza = pallina.Y();
+		fy = pallina.M() * -9.81;
+		fIdroAria = k1Aria * r * abs(pallina.Vy()) + k2Aria * r * r * (pallina.Vy() * pallina.Vy());
+		fIdroAcqua = k1Acqua * r * abs(pallina.Vy()) + k2Acqua * r * r * (pallina.Vy() * pallina.Vy());
+		fArchimedeAcqua = 1000 * volume * 9.81;
 		while (pallina.Y() >= 0) {
-			double forzaDaUsare = fy + fIdroAria;
+			forzaDaUsare = fy + fIdroAria;
 			pallina.calcolaPosizione(0, forzaDaUsare, 0.001);
 			fIdroAria = k1Aria * r * abs(pallina.Vy()) + k2Aria * r * r * (pallina.Vy() * pallina.Vy());
 			if (pallina.Y() <= 0) {
@@ -32,7 +39,7 @@ int main() {
 			altezza = pallina.Y();
 		}
 		while (pallina.Y() <= 0 && pallina.Y()> -150) {
-			double forzaDaUsare = fy + fIdroAcqua + fArchimedeAcqua;
+			forzaDaUsare = fy + fIdroAcqua + fArchimedeAcqua;
 			pallina.calcolaPosizione(0, forzaDaUsare, 0.001);
 			fIdroAcqua = k1Acqua * r * abs(pallina.Vy()) + k2Acqua * r * r * (pallina.Vy() * pallina.Vy());
 			if (altezza > pallina.Y()) {
